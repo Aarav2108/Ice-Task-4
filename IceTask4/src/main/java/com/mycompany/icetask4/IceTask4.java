f20298ed2197a2837b6ec034d97abecda94e28dd
Aarav Shahil Narotam <ST10439143@myemeris.edu.za>
2026-09-18 23:20:20 +0200

    Returns true if the sum of the first five digits, mod 10, equals the sixth digit

--- IceTask4/src/main/java/com/mycompany/icetask4/IceTask4.java
+++ IceTask4/src/main/java/com/mycompany/icetask4/IceTask4.java
@@ -6,6 +6,27 @@ import java.io.*;
 import java.util.Scanner;
 
 public class IceTask4 {
+    
+    public static boolean isValid(String account) {
+        if (account.length() != 6) {
+            return false;
+        }
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            char c = account.charAt(i);
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+            sum += c - '0';
+        }
+        char last = account.charAt(5);
+        if (!Character.isDigit(last)) {
+            return false;
+        }
+        return (sum % 10) == (last - '0');
+    }
+    
+    
 
     public static void main(String[] args) {
        
