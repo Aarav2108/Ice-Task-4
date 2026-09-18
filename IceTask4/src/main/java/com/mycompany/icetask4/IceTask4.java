46362d78536bb0890a665faccc152e2b0957f048
Aarav Shahil Narotam <ST10439143@myemeris.edu.za>
2026-09-18 23:16:47 +0200

    imported my tools

--- IceTask4/src/main/java/com/mycompany/icetask4/IceTask4.java
+++ IceTask4/src/main/java/com/mycompany/icetask4/IceTask4.java
@@ -1,16 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.icetask4;
 
-/**
- *
- * @author aarav
- */
+import java.io.*;
+import java.util.Scanner;
+
 public class IceTask4 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       
     }
 }
