c2d8abd5cf9a1116f0e5f2158ef7b62b30caadb6
Aarav Shahil Narotam <ST10439143@myemeris.edu.za>
2026-09-18 23:12:14 +0200

    Created the project

---
+++ IceTask4/src/main/java/com/mycompany/icetask4/IceTask4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.icetask4;
+
+/**
+ *
+ * @author aarav
+ */
+public class IceTask4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
